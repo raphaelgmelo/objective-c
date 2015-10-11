31c7586c3ca408856f1691f20da85fe27d2dfbc3
Raphael Melo <raphaelgmelo@gmail.com>
2015-10-11 20:41:12 -0300

    dynamic-typing

---
+++ dynamic_type/MyFirstObjectiveCProgram/MyFirstObjectiveCProgram/Shape.h
@@ -0,0 +1,16 @@
+//
+//  Shape.h
+//  MyFirstObjectiveCProgram
+//
+//  Created by Raphael Melo on 11/10/15.
+//  Copyright © 2015 treehouse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Shape : NSObject
+
+-(float)area;
+-(NSString *) description;
+
+@end
---
+++ dynamic_type/MyFirstObjectiveCProgram/MyFirstObjectiveCProgram/Shape.m
@@ -0,0 +1,22 @@
+//
+//  Shape.m
+//  MyFirstObjectiveCProgram
+//
+//  Created by Raphael Melo on 11/10/15.
+//  Copyright © 2015 treehouse. All rights reserved.
+//
+
+#import "Shape.h"
+
+@implementation Shape
+
+-(float)area{
+    return -1;
+}
+
+-(NSString *) description{
+    NSString *descriptionString = @"I am a Shape!";
+    return descriptionString;
+};
+
+@end
